d89e0c070d456e40994d9c12df40af6bb9b25187
galihap76 <g4lihanggoro@gmail.com>
2021-08-13 16:39:49 +0700

    update app

--- script-app4.js
+++ script-app4.js
@@ -45,29 +45,4 @@ function mainkan2(e){
 }
 
 
-//const form = document.querySelector("form");
-//
-//const text = form.querySelector(" textarea");
-//
-//const speak = (message) => {
-//  var speech = new SpeechSynthesisUtterance();
-//
-//  // Set the text and voice attributes.
-//  speech.text = message;
-//  speech.volume = 1;
-//  speech.rate = 1;
-//  speech.pitch = 1;
-//
-//  window.speechSynthesis.speak(speech);
-//};
-//
-//form.addEventListener("submit", (e) => {
-//  e.preventDefault();
-//  speak(text.value);
-//});
-
-
-
-
-
 
